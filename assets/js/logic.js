beeb2bdf90ce9ecb073571405b7fda722ce1853d
mibrahim234 <mohamm1417@gmail.com>
2021-09-07 00:24:29 -0400

    fixed hide button

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,6 @@
 var timerEl = document.querySelector("#timer");
 var startBtn = document.querySelector("#start");
-var startPage = document.querySelector(".start");
+var startPage = document.querySelector("#start-screen");
 var openingPg = document.querySelector("#opening-page");
 var questionsPg = document.querySelector("#questions-page");
 var highscoresBtn = document.querySelector("#highscores");
@@ -17,13 +17,42 @@ var goBackBtn = document.querySelector("go-back");
 var clearBtn = document.querySelector("#clear-highscores");
 var currentQuestionIndex = 0;
 var score = 0;
+var questions = [
+  {
+      title: "Commonly used data types DO NOT include?",
+      choices: ["Strings", "Boolean", "Alerts", "Numbers"],
+      answer: "Alerts"
+  },
+  {
+      title: "The condition in an if/else statement is enclosed with ____.",
+      choices: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
+      answer: "Curly Brackets"
+  },
+  {
+      title: "Arrays in JavaScript can be used to store ___.",
+      choices: ["Numbers and strings", "Other Arrays", "Boolean", "All of the above"],
+      answer: "All of the above"
+  },
+  {
+      title: "String values must be enclosed within___ when being assigned to variables",
+      choices: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+      answer: "Quotes"
+  },
+  {
+      title: "A very useful tool used during development and debugging for printing content to the debugger is:", 
+      choices: ["JavaScript", "Terminal/Bash", "For loops","Console.log"],
+      answer: "Console.log"
+  },
+];
 
 // Timer that counts down from 100
 function countdown() {
     var timeLeft = 100;
 
-    // questions[0].answer is an array that you can loop over 
-    startPage.classList.add("hide");
+ questionEl.textContent = questions[0].title;
+  //  questionEl.textContent = questions[0].choices;
+     //questionEl.textContent = questions[0].answer;
+     startPage.classList.add("hide");
     questionsPg.classList.add("show");
     var timeInterval = setInterval(function() {
       if (timeLeft > 1) {
@@ -59,20 +88,21 @@ function clear () {
 
 startBtn.addEventListener("click", function() {
   for (var i = 0; i < questions.length; i++) {
-    
+    console.log(questions[i]);
   }
   // target question card and change the css to show display block 
   //questionsPg
-  questionsPg.setAttribute("style", "display:block;");
+  questionsPg.setAttribute("style", "display: block;");
 
   // the start page will be hidden
  // startPage
- startPage.setAttribute("style", "display:hide;");
+ startPage.setAttribute("style", "display: none;");
+ submitHighscoresPg.setAttribute("style", "display:none;");
 
 countdown();
 
 //runBuddy(); 
 
 
-  questions();
+  // questions();
 });
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,27 +1,27 @@
-var questions = [
-    {
-        title: "Commonly used data types DO NOT include?",
-        choices: ["Strings", "Boolean", "Alerts", "Numbers"],
-        answer: "Alerts"
-    },
-    {
-        title: "The condition in an if/else statement is enclosed with ____.",
-        choices: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
-        answer: "Curly Brackets"
-    },
-    {
-        title: "Arrays in JavaScript can be used to store ___.",
-        choices: ["Numbers and strings", "Other Arrays", "Boolean", "All of the above"],
-        answer: "All of the above"
-    },
-    {
-        title: "String values must be enclosed within___ when being assigned to variables",
-        choices: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
-        answer: "Quotes"
-    },
-    {
-        title: "A very useful tool used during development and debugging for printing content to the debugger is:", 
-        choices: ["JavaScript", "Terminal/Bash", "For loops","Console.log"],
-        answer: "Console.log"
-    },
-];
+// var questions = [
+//     {
+//         title: "Commonly used data types DO NOT include?",
+//         choices: ["Strings", "Boolean", "Alerts", "Numbers"],
+//         answer: "Alerts"
+//     },
+//     {
+//         title: "The condition in an if/else statement is enclosed with ____.",
+//         choices: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
+//         answer: "Curly Brackets"
+//     },
+//     {
+//         title: "Arrays in JavaScript can be used to store ___.",
+//         choices: ["Numbers and strings", "Other Arrays", "Boolean", "All of the above"],
+//         answer: "All of the above"
+//     },
+//     {
+//         title: "String values must be enclosed within___ when being assigned to variables",
+//         choices: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+//         answer: "Quotes"
+//     },
+//     {
+//         title: "A very useful tool used during development and debugging for printing content to the debugger is:", 
+//         choices: ["JavaScript", "Terminal/Bash", "For loops","Console.log"],
+//         answer: "Console.log"
+//     },
+// ];
